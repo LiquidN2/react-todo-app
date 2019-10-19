d7c991f68a7c15ab88a56f533c64d7488761a57d
Hugh Nguyen <hhnguyen255@gmail.com>
2019-10-19 13:30:35 +1100

    Updates

---
+++ precache-manifest.2e9b3c8e4c9d0a001f8260de211b77a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97046e3c878c961f7bd1c6e3b07c33e",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "3ea79f0668f271b31e54",
+    "url": "/react-todo-app/static/css/main.02c17f32.chunk.css"
+  },
+  {
+    "revision": "63c13678f1f950d39bd6",
+    "url": "/react-todo-app/static/js/2.984a43c5.chunk.js"
+  },
+  {
+    "revision": "3ea79f0668f271b31e54",
+    "url": "/react-todo-app/static/js/main.4c5c0d13.chunk.js"
+  },
+  {
+    "revision": "ea31a015116ac88b5e13",
+    "url": "/react-todo-app/static/js/runtime-main.b40104c2.js"
+  }
+]);
